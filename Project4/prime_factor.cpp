6402fe2f8bdba469f0e526de18ea20590230ece7
yaesolKim <kimyaesol@gmail.com>
2025-07-23 10:05:58 +0900

    [refactoring] 59

--- Project4/prime_factor.cpp
+++ Project4/prime_factor.cpp
@@ -7,21 +7,24 @@ public:
 	vector<int> of(int num) {
 		vector<int> result = {};
 		if (num >1) {
+			int divisor = 2;
 			if (num == 4) {
-				while (num % 2 == 0) {
-					result.push_back(2);
-					num /= 2;
+				while (num % divisor == 0) {
+					result.push_back(divisor);
+					num /= divisor;
 				}
 			}
 			else if (num == 6) {
-				while (num % 2 == 0) {
-					result.push_back(2);
-					num /= 2;
+				while (num % divisor == 0) {
+					result.push_back(divisor);
+					num /= divisor;
 				}
-				while (num % 3 == 0) {
-					result.push_back(3);
-					num /= 3;
+				divisor++;
+				while (num % divisor == 0) {
+					result.push_back(divisor);
+					num /= divisor;
 				}
+				divisor++;
 			}
 			else {
 				result.push_back(num);
