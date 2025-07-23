73fc836f60c8e52f3dd40b92c834cb11f6b6bdf2
yaesolKim <kimyaesol@gmail.com>
2025-07-23 10:04:29 +0900

    [refactoring] 57

--- Project4/prime_factor.cpp
+++ Project4/prime_factor.cpp
@@ -14,8 +14,14 @@ public:
 				}
 			}
 			else if (num == 6) {
-				result.push_back(2);
-				result.push_back(3);
+				while (num % 2 == 0) {
+					result.push_back(2);
+					num /= 2;
+				}
+				while (num % 3 == 0) {
+					result.push_back(3);
+					num /= 3;
+				}
 			}
 			else {
 				result.push_back(num);
