ff322290811dcaaab3a9724cf3474193a87b26ae
yaesolKim <kimyaesol@gmail.com>
2025-07-23 09:56:03 +0900

    [refactoring] 49

--- Project4/prime_factor.cpp
+++ Project4/prime_factor.cpp
@@ -7,10 +7,10 @@ public:
 	vector<int> of(int num) {
 		vector<int> result = {};
 		if (num == 2) {
-			result.push_back(2);
+			result.push_back(num);
 		}
 		if (num == 3) {
-			result.push_back(3);
+			result.push_back(num);
 		}
 		return result;
 	}
