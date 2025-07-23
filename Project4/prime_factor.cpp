f33e9fe884c42bff15660ea3d68ae528f749eab7
yaesolKim <kimyaesol@gmail.com>
2025-07-23 10:16:52 +0900

    [refactoring] 67

--- Project4/prime_factor.cpp
+++ Project4/prime_factor.cpp
@@ -6,18 +6,10 @@ class PrimeFactors {
 public:
 	vector<int> of(int num) {
 		vector<int> result = {};
-		if (num >1) {
-			int divisor = 2;
-			if (num == 4 ||num == 6 || num == 9 || num == 12) {
-				for (divisor = 2; num > 1; divisor++) {
-					while (num % divisor == 0) {
-						result.push_back(divisor);
-						num /= divisor;
-					}
-				}
-			}
-			else {
-				result.push_back(num);
+		for (int divisor = 2; num > 1; divisor++) {
+			while (num % divisor == 0) {
+				result.push_back(divisor);
+				num /= divisor;
 			}
 		}
 
