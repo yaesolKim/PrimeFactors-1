261d06503f8acb2726f20427de6bcfe3c7ef22e9
yaesolKim <kimyaesol@gmail.com>
2025-07-23 10:13:51 +0900

    [refactoring] 64

--- Project4/prime_factor.cpp
+++ Project4/prime_factor.cpp
@@ -8,13 +8,7 @@ public:
 		vector<int> result = {};
 		if (num >1) {
 			int divisor = 2;
-			if (num == 4) {
-				while (num % divisor == 0) {
-					result.push_back(divisor);
-					num /= divisor;
-				}
-			}
-			else if (num == 6 || num == 9) {
+			if (num == 4 ||num == 6 || num == 9) {
 				for (divisor = 2; num > 1; divisor++) {
 					while (num % divisor == 0) {
 						result.push_back(divisor);
