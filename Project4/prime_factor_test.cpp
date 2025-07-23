b72c206d6cc8b4e130f0c44897e14f79511dad9d
yaesolKim <kimyaesol@gmail.com>
2025-07-23 09:52:07 +0900

    [feature] 45

--- Project4/prime_factor_test.cpp
+++ Project4/prime_factor_test.cpp
@@ -6,3 +6,10 @@ TEST(PrimeFactors, tc1) {
 	vector<int> expected = {};
 	EXPECT_EQ(expected, prime_factor.of(1));
 }
+
+TEST(PrimeFactors, tc2) {
+	PrimeFactors prime_factor;
+	vector<int> expected = {2};
+	EXPECT_EQ(expected, prime_factor.of(2));
+}
+
